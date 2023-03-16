a1921ef05bd20a3393ade59c89ee61bb7de49b5d
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-16 17:14:24 -0300

    Add project files.

---
+++ DiamanteApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace DiamanteApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanhoDiamante;
+     
+
+            Console.WriteLine("Digite um número para ver o desenho de diamentes: ");
+            tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
+
+            int quantidadeBranco = (tamanhoDiamante - 1) / 2;
+            int quantidadeX = 1;
+
+            for (int i = 0; i < tamanhoDiamante / 2; i++)
+            {
+                for (int j = 0; j < quantidadeBranco; j++)
+                {
+                    Console.Write(" ");
+                }
+                quantidadeBranco--;
+                for (int k = 0; k < quantidadeX; k++)
+                {
+                    Console.Write("X");
+                }
+                quantidadeX += 2;
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < tamanhoDiamante; i++)
+            {
+                Console.Write("X");
+            }
+            Console.WriteLine();
+
+            quantidadeBranco = 1;
+            quantidadeX -= 2;
+
+            for (int i = 0; i < tamanhoDiamante / 2; i++)
+            {
+                for (int j = 0; j < quantidadeBranco; j++)
+                {
+                    Console.Write(" ");
+                }
+                quantidadeBranco++;
+                for (int k = 0; k < quantidadeX; k++)
+                {
+                    Console.Write("X");
+                }
+                quantidadeX -= 2;
+                Console.WriteLine();
+            }
+        }
+    }
+}
